36bf5791b03652a80561bb0c91d9bfdc9293857b
Andrei <hristea52@gmail.com>
2017-02-09 22:56:41 -0800

    made them files - ctrl, model, view frag

---
+++ CodeIgniter3.1-starter3-master/application/models/Parts.php
@@ -0,0 +1,29 @@
+<?php
+
+class Parts extends CI_Model {
+
+  private $partId;
+  private $partCode;
+  private $caCode;    // Cert of Authenticity
+  private $plantBuiltAt;
+  private $dateBuilt;
+  private $timeBuilt;
+
+  public function __construct() {
+    parent::__construct();
+
+  }
+
+  public function getModel(){
+
+  }
+
+  public function getRobotLine(){
+
+  }
+
+  public function getPartType(){
+    
+  }
+
+}
