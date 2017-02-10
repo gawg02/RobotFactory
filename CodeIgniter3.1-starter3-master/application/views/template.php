0d333d9d7cd33d126bd768323aa7bf4dfa0677e5
Andrei <hristea52@gmail.com>
2017-02-09 23:10:05 -0800

    gittin some libraries in here

--- CodeIgniter3.1-starter3-master/application/views/template.php
+++ CodeIgniter3.1-starter3-master/application/views/template.php
@@ -5,6 +5,10 @@
 		<title>{pagetitle}</title>
         <meta HTTP-EQUIV="Content-Type" CONTENT="text/html; charset=UTF-8"/>
         <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+
+        <!-- A little bit of bootstrap in my life --> 
+        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" type="text/css" rel="stylesheet" />
+
         <link rel="stylesheet" type="text/css" href="/assets/css/default.css"/>
 	</head>
 	<body>
@@ -13,5 +17,19 @@
 			<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. 
 				{ci_version}</p>
         </div>
+
+  <footer>
+  
+      <!-- A little bit of jquery by my side --> 
+      <script src="https://code.jquery.com/jquery-3.1.1.min.js" 
+      integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8="
+      crossorigin="anonymous"></script>
+
+      <!-- A little bit more of bootstrap is all i need -->
+      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" 
+      integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+  </footer>
+
 	</body>
 </html>
