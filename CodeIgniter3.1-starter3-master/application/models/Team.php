6824a7854bb914afbc8e7935fe3436ca001892e3
Jay Coughlan <JaysCogs@gmail.com>
2017-02-10 18:30:45 -0800

    More features on about, changed changelog

--- CodeIgniter3.1-starter3-master/application/models/Team.php
+++ CodeIgniter3.1-starter3-master/application/models/Team.php
@@ -9,6 +9,38 @@ class Team extends CI_Model
             'face' => 'jcoughlan.jpg',
             'quote' => 'Yes, I exist',
             'blurb' => 'jblurb.txt'
+        ),
+
+        array(
+            'id' => '2',
+            'name' => 'Bryn Newman',
+            'face' => 'jcoughlan.jpg',
+            'quote' => 'Howdy do?',
+            'blurb' => 'jblurb.txt'
+        ),
+
+        array(
+            'id' => '3',
+            'name' => 'Greg Little',
+            'face' => 'jcoughlan.jpg',
+            'quote' => 'Gwent, man, gwent.',
+            'blurb' => 'jblurb.txt'
+        ),
+
+        array(
+            'id' => '4',
+            'name' => 'Andrei Hristea',
+            'face' => 'jcoughlan.jpg',
+            'quote' => 'Sweet home Alabama!',
+            'blurb' => 'jblurb.txt'
+        ),
+
+        array(
+            'id' => '5',
+            'name' => 'Renz Dionglay',
+            'face' => 'jcoughlan.jpg',
+            'quote' => '...',
+            'blurb' => 'jblurb.txt'
         )
     );
 
--- CodeIgniter3.1-starter3-master/application/views/about.php
+++ CodeIgniter3.1-starter3-master/application/views/about.php
@@ -1,12 +1,15 @@
+<head>
+    <style>
+        img {height:200px; width:auto;}
+    </style>
+</head>
+
 <div class='col-md-12' id='content'>
     <h1>About Team Elderberry!</h1>
 
     <p>This is a student project for COMP4711 at BCIT. There is no affiliation with us and any company, fictional or otherwise.</p>
-    <table border=0>
-        <tr>
-            {team}
-            <div class="span4"><img src="/assets/images/faces/{face}" title="{name}"/></a></div>
-            {/team}
-        </tr>
-    </table>
+    {team}
+    <span class = "teammembers"><img src="/assets/images/faces/{face}" title="{quote}"/></a></span>
+    <h3>{name}</h3>
+    {/team}
 </div>
