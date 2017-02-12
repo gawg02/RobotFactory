a16411b3f78b9a145293fb45abe42294ee92f3d0
Jay Coughlan <JaysCogs@gmail.com>
2017-02-11 16:09:53 -0800

    Further work done

--- CodeIgniter3.1-starter3-master/application/views/about.php
+++ CodeIgniter3.1-starter3-master/application/views/about.php
@@ -2,12 +2,12 @@
     <link rel='stylesheet' type="text/css" href="/assets/css/about.css">
 </head>
 
-<span class='span4' id='content'>
+<span class='col-md-12' id='content'>
     <h1>About Team Elderberry!</h1>
 
     <p>This is a student project for COMP4711 at BCIT. There is no affiliation with us and any company, fictional or otherwise.</p>
     {team}
-    <span class = "teammembers"><img src="/assets/images/faces/{face}" title="{quote}"/></a></span>
+    <span class = 'col-md-3' id = "teammembers"><img src="/assets/images/faces/{face}" title="{quote}"/></a></span>
     <h3>{name}</h3>
     {/team}
 </span>
