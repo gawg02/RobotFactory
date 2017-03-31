7cb3482e0fec94760dddaebd0ece00ba47c7e523
Andrei <hristea52@gmail.com>
2017-03-30 22:43:28 -0700

    Quick fix to bootstrap initialization in homepage.php No other changes

--- application/views/homepage.php
+++ application/views/homepage.php
@@ -14,8 +14,6 @@
                 Copyright &copy; 2015-2016,  <a href="mailto:someone@somewhere.com">Me</a>.
             </div>
         </div>
-        <script src="/assets/js/jquery-1.11.1.min.js"></script>
-        <script src="/assets/js/bootstrap.min.js"></script>
     </body>
 	
 </html>
